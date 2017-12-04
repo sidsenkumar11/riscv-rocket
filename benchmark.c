99a87c624373caa41995f123c12ba53a875ea823
Nicholas Liu <nickliu96@gmail.com>
2017-12-04 18:08:38 -0500

    Add more iterations and time

--- benchmark.c
+++ benchmark.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <time.h>
 
 int main(void) {
   int mat1[4][4] = { {1, 1, 1, 1},
@@ -13,12 +14,14 @@ int main(void) {
   int res[4][4];
 
   printf("Hello world!\\n");
-  int i, j;
-  for (i = 0; i < 4; i++) {
-    for (j = 0; j < 4; j++) {
-      res[i][j] = 0;
-      for (k = 0; k < 4; k++) {
-        res[i][j] += mat1[i][k]*mat2[k][j];
+  int cnt, i, j;
+  for (cnt = 0; cnt < 1000; cnt++) {
+    for (i = 0; i < 4; i++) {
+      for (j = 0; j < 4; j++) {
+        res[i][j] = 0;
+        for (k = 0; k < 4; k++) {
+          res[i][j] += mat1[i][k]*mat2[k][j];
+        }
       }
     }
   }
